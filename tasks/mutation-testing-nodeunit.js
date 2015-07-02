aa72e6ce912a0d08700fc4b8863942ab6c8f9d72
skoblins <adam.skoblinski@nsn.com>
2015-07-02 15:22:26 +0200

    run tests in child processes

--- tasks/mutation-testing-nodeunit.js
+++ tasks/mutation-testing-nodeunit.js
@@ -2,21 +2,108 @@
 
 var CopyUtils = require('../utils/CopyUtils'),
     path = require('path'),
-    fs = require('fs'),
     _ = require('lodash'),
     nodeunit = require('nodeunit'),
     TestStatus = require('../lib/TestStatus'),
-    log4js = require('log4js');
-var logger = log4js.getLogger('mutation-testing-nodeunit');
+    log4js = require('log4js'),
+    logger = log4js.getLogger('mutation-testing-nodeunit'),
+    Promise = require('bluebird'),
+    globAsync = Promise.promisify(require("glob"));
+
+Promise.longStackTraces();
+var cp = require("child_process");
+var fs = Promise.promisifyAll(require("fs"));
+var E = require("core-error-predicates");
+var rimrafAsync = Promise.promisify(require("rimraf"));
+
+function runAsync(execStr) {
+    return new Promise(function(resolve, reject) {
+        var stdout = [];
+        var stderr = [];
+        var elements = execStr.split(/\s+/g);
+        var command = elements.shift();
+        var args = elements;
+
+        var newProcess = cp.spawn(command, args);
+        newProcess.stdout.on("data", function(buffer) {
+            process.stdout.write(buffer);
+            stdout.push(buffer);
+        });
+        newProcess.stderr.on("data", function(buffer) {
+            process.stderr.write(buffer);
+            stderr.push(buffer);
+        });
+        newProcess.on("close", function(code) {
+            stdout = Buffer.concat(stdout).toString("utf8");
+            stderr = Buffer.concat(stderr).toString("utf8");
+            if (code === 0) {
+                resolve({
+                    stdout: stdout,
+                    stderr: stderr
+                });
+            } else {
+                var error = new Error(execStr + " exited with failure code: " + code);
+                error.stdout = stdout;
+                error.stderr = stderr;
+                error.command = execStr;
+                reject(error);
+            }
+        });
+    }).catch(function(e) {
+        e.command = execStr;
+        throw e;
+    });
+}
+
+
 
 exports.init = function(grunt, opts) {
     if(opts.testFramework !== 'nodeunit') {
         return;
     }
 
-    var testFiles = opts.specs;
-    if(testFiles.length === 0) {
-        logger.warn('No test files configured; opts.specs is empty');
+    var commandLineOptMutate = grunt.option("mutationTest:options:mutate");
+    var commandLineOptCode = grunt.option("mutationTest:options:code");
+    var commandLineOptCodeAdditional = grunt.option("mutationTest:options:code:additional");
+
+    if(commandLineOptMutate){
+        opts.mutate = grunt.file.expand(commandLineOptMutate);
+        logger.info('Overriding opts.mutate with %s', opts.mutate);
+    };
+
+    if(commandLineOptCode){
+        opts.code = grunt.file.expand(commandLineOptCode);
+        logger.info('Overriding opts.code with %s', opts.code);
+    };
+
+    if(commandLineOptCodeAdditional){
+        commandLineOptCodeAdditional = commandLineOptCodeAdditional.split(',');
+        commandLineOptCodeAdditional.forEach(function(minimaxExpr){
+            var expanded = grunt.file.expand(minimaxExpr);
+            logger.info('Appending opts.code with %s', expanded);
+            opts.code = opts.code.concat(expanded);
+        })
+        logger.info('Overriding opts.code with %s', opts.code);
+    };
+
+    function expandSpecs() {
+        var specPattern = grunt.option("mutationTest:options:specs") || "test/**/*.js";
+        var globResult = globAsync(specPattern);
+        return Promise
+            .join(globResult)
+            .return(globResult);
+    }
+
+    var expandedSpecs = expandSpecs();
+
+    function runTests(specs){
+        return specs.each(function(file) {
+            var name = path.basename(file).replace(path.extname(file), "");
+            var p = path.join(process.cwd(), file);
+            // var p = file;
+            // logger.error(p);
+            return runAsync("nodeunit --reporter minimal " + p);
+        });
     }
 
     opts.before = function(doneBefore) {
@@ -24,7 +111,8 @@ exports.init = function(grunt, opts) {
             doneBefore();
         } else {
             // Find which files are used in the unit test such that they can be copied
-            CopyUtils.copyToTemp(opts.code.concat(opts.specs), 'mutation-testing').done(function(tempDirPath) {
+            CopyUtils.copyToTemp(opts.code.concat(opts.specs), 'mutation-testing')
+            .done(function(tempDirPath) {
                 logger.trace('Copied %j to %s', opts.code.concat(opts.specs), tempDirPath);
 
                 // create symlinks in tmpDirPath, if set
@@ -33,47 +121,38 @@ exports.init = function(grunt, opts) {
                         var srcPath = path.resolve(path.join(opts.basePath, folder));
                         var tgtPath = path.join(tempDirPath, folder);
                         fs.symlinkSync(srcPath, tgtPath);
+                        logger.info('Created symbolik link: %s -> %s', srcPath, tgtPath);
                     })
                 };
 
                 // Set the basePath relative to the temp dir
                 opts.basePath = path.join(tempDirPath, opts.basePath);
 
-                testFiles = _.map(testFiles, function(file) {
-                    return path.join(tempDirPath, file);
-                });
-
-                // Overwrite the 'mutate' option with the commandline one, if given
-                var commandLineOptMutate = grunt.option("mutationTest:options:mutate");
-                if(commandLineOptMutate){
-                    commandLineOptMutate = grunt.file.expand(commandLineOptMutate);
-                    opts.mutate = commandLineOptMutate;
-                };
-
                 // Set the paths to the files to be mutated relative to the temp dir
                 opts.mutate = _.map(opts.mutate, function(file) {
                     return path.join(tempDirPath, file);
                 });
 
+                process.chdir(tempDirPath);
 
-                doneBefore();
+                runTests(expandedSpecs).then(function(){
+                    doneBefore();
+                })
+                .catch(function(error) {
+                    logger.error('Tests don\'t pass without mutations!');
+                    process.exit(-1);
+                });
             });
         }
     };
 
     opts.test = function(done) {
-        nodeunit.on('complete', function(name, assertions){
-            if (assertions.failures() === 0) {
-                done(TestStatus.SURVIVED);
-            } else {
-                done(TestStatus.KILLED);
-            };
-        });
-
-        try{
-            nodeunit.runFiles(testFiles, {});
-        } catch (error){
+        runTests(expandedSpecs).then(function(){
+            done(TestStatus.SURVIVED);
+        })
+        .catch(function(error) {
+            logger.error(error);
             done(TestStatus.KILLED);
-        }
+        });
     }
 }
